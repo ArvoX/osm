18aaa49a7c97447034c0148e7d2375543794178f
ArvoX <github@arvox.dk>
2012-02-17 14:58:20 +0100

    rettet test til at virke paa linux

--- g1/braunseq/testbraunseq.c
+++ g1/braunseq/testbraunseq.c
@@ -4,6 +4,7 @@
 
 void printTree(Tree tree){
     int treeSize = size(tree);
+    printf("%i\n", treeSize);
     for (int i=0; i<treeSize; i++) {
         printf("index %i = %f\n",i,lookup(tree, i));
     }
@@ -11,25 +12,24 @@ void printTree(Tree tree){
 
 
 int main(){
-	Tree tree;
-    *tree = NULL;
+    void *a = NULL;
+    Tree tree = (Tree)(&a);
     
-	printf("instantiate tree..\n");
-	printf("Adding elements\n");
-	addL(tree,1);
-	addL(tree,2);
-	addL(tree,3);
-	addL(tree,4);
-	addL(tree,5);
-	addL(tree,6);
-	addL(tree,7);
-	addL(tree,8);
-	//printf("instantiate tree..\n");
-	//printf("instantiate tree..\n");
-	//printf("instantiate tree..\n");
-	//printf("Size of tree = %d\n",size(tree));
+    printf("instantiate tree..\n");
+    printf("Adding elements\n");
+    addL(tree,1);
+    addL(tree,2);
+    addL(tree,3);
+    addL(tree,4);
+    addL(tree,5);
+    addL(tree,6);
+    addL(tree,7);
+    addL(tree,8);
+    //printf("instantiate tree..\n");
+    //printf("instantiate tree..\n");
+    //printf("instantiate tree..\n");
+    //printf("Size of tree = %d\n",size(tree));
     printTree(tree);
     
-
-	return 0;
+    return 0;
 }
