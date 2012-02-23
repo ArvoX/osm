98fa961b2aeea65e3633f726741e2608eda9208e
MadsL <ladsmund@gmail.com>
2012-02-23 17:27:12 +0100

    ?

--- g2/buenos/proc/syscall.c
+++ g2/buenos/proc/syscall.c
@@ -89,7 +89,7 @@ void syscall_handle(context_t *user_context)
 			break;
  */		default:
 			kprintf("syscall no: %d\n",user_context->cpu_regs[MIPS_REGISTER_A0]);	
-			DEBUG("debugsyscall","test med debug print\n");
+			DEBUG("debuginit","test med debug print\n");
 			KERNEL_PANIC("Unhandled system call\n");
 	}
 
