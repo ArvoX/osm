e63ff1fe9d3273b44ecbae0da07aa30ea5dae47c
ArvoX <github@arvox.dk>
2012-02-23 14:53:12 +0100

    fjerner *.d filer fra git

--- g2/buenos/drivers/metadev.d
+++
@@ -1,4 +0,0 @@
-drivers/metadev.o: drivers/metadev.c drivers/metadev.h lib/types.h drivers/yams.h \
-  drivers/device.h kernel/spinlock.h lib/libc.h lib/types.h \
-  kernel/panic.h kernel/assert.h kernel/panic.h kernel/kmalloc.h \
-  drivers/yams.h kernel/interrupt.h lib/types.h drivers/device.h
