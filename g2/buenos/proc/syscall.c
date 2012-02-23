6a7a91d8a85a76e19d8c8de14f63f3e47f5332be
MadsL <ladsmund@gmail.com>
2012-02-23 16:16:11 +0100

    case SYSCALL_EXIT tilføjet

--- g2/buenos/proc/syscall.c
+++ g2/buenos/proc/syscall.c
@@ -67,6 +67,8 @@ void syscall_handle(context_t *user_context)
 			int pid = process_spawn(file);
 			user_context->cpu_regs[MIPS_REGISTER_V0] = pid;
 			break;
+		case SYSCALL_EXIT:
+            break;
 		case SYSCALL_JOIN:
 			user_context->cpu_regs[MIPS_REGISTER_V0] = 
 				process_join((process_id_t)user_context->cpu_regs[MIPS_REGISTER_A1];            
