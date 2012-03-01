c8dc103524c8dfb59f0d710cf1b7245bb9ee6329
MadsL <ladsmund@gmail.com>
2012-03-01 14:56:12 +0100

    Tilføjelser til lock_cond.c

--- buenos/kernel/lock_cond.c
+++ buenos/kernel/lock_cond.c
@@ -7,16 +7,23 @@
 #include "kernel/lock.h"
 #include "kernel/condition.h"
 #include "kernel/sleepq.h"
+#include "kernel/interrupt.h"
 
 
 void condition_init(cond_t *cond){
 	
 }
 void condition_wait(cond_t *cond, lock_t *lock){
-		
+	
+	
+    interrupt_status_t intr_status = _interrupt_disable();
+
 	sleepq_add(cond);
-	lock_release(lock);
+	lock_release(lock);	
 	thread_switch();
+	
+    _interrupt_set_state(intr_status);
+	
 	lock_acquire(lock);
 	
 }
