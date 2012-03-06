8e18a99959dde12040b3ee457cec5a8e3d328814
ArvoX <github@arvox.dk>
2012-03-06 11:34:43 +0100

    added flatfs to filesystems

--- buenos/fs/filesystems.c
+++ buenos/fs/filesystems.c
@@ -36,12 +36,14 @@
 
 #include "fs/filesystems.h"
 #include "fs/tfs.h"
+#include "fs/flatfs.h"
 #include "drivers/device.h"
 
 /* NULL terminated table of all available filesystems. */
 
 static filesystems_t filesystems[] = {
     {"TFS", &tfs_init},
+    {"FLATFS", &flatfs_init},
     { NULL, NULL} /* Last entry must be a NULL pair. */ 
 };
 
