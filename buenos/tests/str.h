0e5567ecc4768e72bd47e071d129c20d03268b24
ArvoX <github@arvox.dk>
2012-03-08 15:16:46 +0100

    refakturering

---
+++ buenos/tests/str.h
@@ -0,0 +1,11 @@
+#ifndef STR
+#define STR
+
+void write(char *str);
+void writeChar(char c);
+void writeInt(int i);
+int strlen(char *str);
+int strcmp(char *str1, char *str2);
+int readline(char *buf, int len);
+
+#endif
